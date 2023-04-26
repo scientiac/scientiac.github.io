48a5ef99ade8790d1c7abaaaf587bbaf6b426a6e
scientiac <thescientiac@protonmail.ch>
2023-04-26 09:58:41 +0545

    design changes

--- assets/js/themeSelector.js
+++ assets/js/themeSelector.js
@@ -1,28 +1,33 @@
 const favicon = `<?xml version="1.0" encoding="UTF-8"?> <svg width="256" height="256" version="1.1" viewBox="0 0 67.733 67.733" xmlns="http://www.w3.org/2000/svg"> <rect width="67.733" height="67.733" fill="#000" stroke="#0f0" stroke-width="8.4667"/> <text x="15.538762" y="53.988174" font-family="monospace" font-size="10.583px" stroke-width=".26458" word-spacing="0px" style="line-height:1.25" xml:space="preserve"><tspan x="15.538762" y="53.988174" fill="#0f0" font-family="'Liberation Mono'" font-size="61.736px" stroke-width=".26458" style="font-variant-caps:normal;font-variant-east-asian:normal;font-variant-ligatures:normal;font-variant-numeric:normal">$</tspan></text> </svg>`;
 
-const themecolor = `mango`
-
 const originalFaviconColors = {
   stroke: '#000',
   fill: '#0f0'
 }
 const themes = {
-  gruvbox: [
-    "light",
-    "dark"
-  ],
-  solis: [],
-  spaceduck: [],
-  nord: [
-    'light',
-    'dark'
+  genre: [
+    'poetic',
+    'storific',
+    'articalistic'
   ],
   everforest: [
     'light',
     'dark'
   ],
-  gotham: [],
-  dracula: [],
+  gruvbox: [
+    "light",
+    "dark"
+  ],
+  night: [
+    'solis',
+    'spaceduck',
+    'gotham',
+    'dracula'
+  ],
+  nord: [
+    'light',
+    'dark'
+  ],
   one: [
     'light',
     'dark'
@@ -39,8 +44,8 @@ const themes = {
   ],
 }
 const defaultThemes = {
-  light: "gruvbox-light",
-  dark: "gruvbox-dark"
+  light: "everforest-light",
+  dark: "everforest-dark"
 }
 const prefersDark = (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
 let theme = localStorage.theme || (prefersDark ? defaultThemes.dark : defaultThemes.light);
